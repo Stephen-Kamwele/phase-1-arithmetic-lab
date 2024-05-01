8651295530bde93fc878ca4146df64aeffebf3b8
Kamwele-hub <stephen.kamwele@student.moringaschool.com>
2024-05-01 15:15:20 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+function multiplyNums (num1, num2){
+     let num1 = 31
+     let num2 = 2
+     const result = num1 * num2
+     return result
+}
+
+function generateRandom() {
+    return Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+}
+
+
+function calculateMod(num1, num2) {
+    return num1 % num2;
+}
+
+
+function findMax(...nums) {
+    return Math.max(...nums);
+}
